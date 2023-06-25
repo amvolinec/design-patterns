a9bee23aea0fbb6e3c946dcea77c34f0f69c4fda
irynasluchova <irynasluchova@gmail.com>
2023-06-25 19:30:17 +0300

    Adapter added

---
+++ Structural/Adapter/run.php
@@ -0,0 +1,23 @@
+<?php
+
+function clientCode(Notification $notification)
+{
+    // ...
+
+    echo $notification->send("Website is down!",
+        "<strong style='color:red;font-size: 50px;'>Alert!</strong> " .
+        "Our website is not responding. Call admins and bring it up!");
+
+    // ...
+}
+
+echo "Client code is designed correctly and works with email notifications:\n";
+$notification = new EmailNotification("avolinec@gmail.com");
+clientCode($notification);
+echo "\n\n";
+
+
+echo "The same client code can work with other classes via adapter:\n";
+$slackApi = new SlackApi("example.com", "XXXXXXXX");
+$notification = new SlackNotification($slackApi, "Example.com Developers");
+clientCode($notification);
